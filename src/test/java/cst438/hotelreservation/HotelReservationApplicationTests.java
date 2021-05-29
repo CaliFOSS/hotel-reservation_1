9689035c9fb85c5d013055f58d68f8fffc8d71f3
jeffgarrett <jeff2invest@gmail.com>
2021-05-29 13:47:17 -0700

    adding initial files

---
+++ src/test/java/cst438/hotelreservation/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438.hotelreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
